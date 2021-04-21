d77cbef66a313208c4b2ab05186999a9b63e4ce1
Rafael D Silva <neromad@outlook.com>
2021-04-20 23:18:36 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+// jsx = html dentro do javascript 
+// tsx = typescript + jsx ( xml no javascript )
+
+// SSG: static side generation ( utilizaremos esse modelo)
+
+export default function Home(props) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  )
+}
+
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, 
+  }
+}
